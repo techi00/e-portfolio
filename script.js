38bc0acdb024a04e198d87273d6118930c1acb15
Chinoy techi <91409267+techi00@users.noreply.github.com>
2023-03-12 17:01:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+const navbar = document.querySelector(".navbar");
+const navbarOffsetTop = navbar.offsetTop;
+const sections = document.querySelectorAll("section");
+const navbarLinks = document.querySelectorAll(".navbar-link");
+const progress = document.querySelector(".progress-bars-wrapper");
+const progressBarPercents = [90, 80, 75, 80, 70, 90, 50,80,75];
+
+window.addEventListener("scroll", () => {
+  mainFn();
+});
+
+const mainFn = () => {
+  if (window.pageYOffset >= navbarOffsetTop) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+
+  sections.forEach((section, i) => {
+    if (window.pageYOffset >= section.offsetTop - 10) {
+      navbarLinks.forEach((navbarLink) => {
+        navbarLink.classList.remove("change");
+      });
+      navbarLinks[i].classList.add("change");
+    }
+  });
+
+  if (window.pageYOffset + window.innerHeight >= progress.offsetTop) {
+    document.querySelectorAll(".progress-percent").forEach((el, i) => {
+      el.style.width = `${progressBarPercents[i]}%`;
+      el.previousElementSibling.firstElementChild.textContent =
+        progressBarPercents[i];
+    });
+  }
+};
+
+mainFn();
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
+const banner=document.getElementsByClassName('banner')[0];
+const blocks=document.getElementsByClassName('blocks');
+for(var i=1;i<400;i++ ){
+  banner.innerHTML +="<div class='blocks'></div>";
+  blocks[i].style.animationDelay=`${i*0.06}s`;
+}
